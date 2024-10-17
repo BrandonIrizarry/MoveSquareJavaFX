167b97225259e7ef50df2d6e8388c48bbe467b24
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-16 20:39:12 -0400

    Pull in backend dependency from custom Maven repository!

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,13 +8,22 @@ group = "xyz.brandonirizarry"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/BrandonIrizarry/MoveSquareBackend")
+        credentials {
+            username = System.getenv("GITHUB_USER")
+            password = System.getenv("GITHUB_PAT")
+        }
+    }
+
     mavenCentral()
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation(files("libs/MoveSquareBackend.jar"))
+    implementation("xyz.brandonirizarry:move-square-backend:1.0")
 }
 
 application {
